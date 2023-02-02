06c00f7fea5bf35da05d083b5d0c6d47c3b797c2
Victor Thompson <kelechithompson26@gmail.com>
2023-02-02 10:59:09 +0100

    add prototpe.findcontact

--- js/script.js
+++ js/script.js
@@ -12,3 +12,10 @@ AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 };
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
