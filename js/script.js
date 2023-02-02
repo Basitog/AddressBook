860a73e4abfa5d7f95445fc04c172f2b9626e0a7
Victor Thompson <kelechithompson26@gmail.com>
2023-02-02 10:56:22 +0100

    add addressbook

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function AddressBook() {
+  this.contacts = {};
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+};
